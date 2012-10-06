6ee542f0f05f649f709c894ed220e930eef348e6
Alexander Herrera <alexcherrera@fullsail.edu>
2012-10-06 10:49:01 -0400

    push the organizer files

--- avfandrappli/College_Organizer/src/com/fullsail/college_organizer/Organizer.java
+++ avfandrappli/College_Organizer/src/com/fullsail/college_organizer/Organizer.java
@@ -9,6 +9,6 @@ public class Organizer extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+        super.loadUrl("file:///../assets/www/index.html");
     }
 }
